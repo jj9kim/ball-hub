38a11bff1a5976818b6808915ace872a2ebed001
jj9kim <kimjoshua355@gmail.com>
2025-09-10 17:16:13 -0400

    added settings to navbar

--- frontend/src/header/Header.tsx
+++ frontend/src/header/Header.tsx
@@ -1,13 +1,14 @@
 import logo from "../img/BallHub.png";
+import settings from "../img/settings.svg"
 
 function Header() {
   return (
       <div className="h-1/10 w-full mt-0 bg-[#1a1a1a] flex">
-          <div className="flex pl-15 bg-[#1a1a1a]">
+          <div className="flex pl-15 bg-[#1a1a1a] pt-1">
               <img src={logo} className="h-20 w-20 right-30 hover:rotate-4" alt="logo" />
-              <h2 className="pt-3.5 font-bold text-white text-5xl hover:text-[#9f9f9f]">BallHub</h2>
+              <h2 className="pt-4 font-bold text-white text-5xl hover:text-[#9f9f9f]">BallHub</h2>
         </div>
-        <div className="pl-20 pt-4">
+        <div className="pl-20 pt-6">
             <form className="max-w-md mx-auto">
                   <label form="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
                   <div className="relative">
@@ -16,13 +17,22 @@ function Header() {
                               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                           </svg>
                       </div>
-                      <input type="search" id="default-search" autoComplete="off" className="focus:outline-none block w-75 p-3 ps-10 text-sm text-white rounded-2xl bg-[#2c2c2c]" placeholder="Search"/>
+                      <input type="search" id="default-search" autoComplete="off" className="focus:outline-none block w-75 p-3 ps-10 text-sm text-white rounded-3xl bg-[#2c2c2c]" placeholder="Search"/>
                   </div>
               </form>
         </div>
-        <div className="pl-70 pt-5">
-            <button className="text-white font-bold text-3xl hover:text-[#9f9f9f]">Table</button>
+        <div className="pl-70 pt-6">
+            <button className="text-white font-bold text-2xl hover:text-[#9f9f9f]">Teams</button>
         </div>
+          <div className="pl-7 pt-6">
+              <button className="text-white font-bold text-2xl hover:text-[#9f9f9f]">Player Stats</button>
+          </div>
+          <div className="pl-7 pt-3">
+              <p className="text-white text-5xl">|</p>
+          </div>
+          <div className="pl-7 pt-6">
+              <img src={settings} alt="settings" className="h-8 w-8 hover:rotate-4"/>
+          </div>
     </div>
   )
 }
