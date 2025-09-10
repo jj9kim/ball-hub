540827a8d6bbca4f7298ca65e276da1a47b45533
jj9kim <kimjoshua355@gmail.com>
2025-09-10 16:37:41 -0400

    added changes to navbar

--- frontend/src/header/Header.tsx
+++ frontend/src/header/Header.tsx
@@ -3,9 +3,9 @@ import logo from "../img/BallHub.png";
 function Header() {
   return (
       <div className="h-1/10 w-full mt-0 bg-[#1a1a1a] flex">
-        <div className="flex pl-15">
-            <img src={logo} className="h-20 w-20 right-30" alt="logo" />
-              <h2 className="pt-3.5 font-bold text-white text-5xl">BallHub</h2>
+          <div className="flex pl-15 bg-[#1a1a1a]">
+              <img src={logo} className="h-20 w-20 right-30 hover:rotate-4" alt="logo" />
+              <h2 className="pt-3.5 font-bold text-white text-5xl hover:text-[#9f9f9f]">BallHub</h2>
         </div>
         <div className="pl-20 pt-4">
             <form className="max-w-md mx-auto">
@@ -16,12 +16,12 @@ function Header() {
                               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                           </svg>
                       </div>
-                      <input type="search" id="default-search" autoComplete="off" className="focus:outline-none block w-75 p-3 ps-10 text-sm text-white rounded-2xl bg-[#2c2c2c]" placeholder="Search" required />
+                      <input type="search" id="default-search" autoComplete="off" className="focus:outline-none block w-75 p-3 ps-10 text-sm text-white rounded-2xl bg-[#2c2c2c]" placeholder="Search"/>
                   </div>
               </form>
         </div>
         <div className="pl-70 pt-5">
-            <button className="text-white font-bold text-3xl">Table</button>
+            <button className="text-white font-bold text-3xl hover:text-[#9f9f9f]">Table</button>
         </div>
     </div>
   )
