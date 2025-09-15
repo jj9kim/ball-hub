5186fb5113345d3707979324b93d53daf8428c31
jj9kim <kimjoshua355@gmail.com>
2025-09-15 15:01:20 -0400

    edited the main matches card

--- frontend/src/assets/Main.tsx
+++ frontend/src/assets/Main.tsx
@@ -3,8 +3,8 @@
 function Main() {
     return (
         <div className="flex justify-center border-4 border-white text-white">
-            <div className="border-4 border-red-500">
-                <input type="date" />
+            <div className="border-4 border-red-500 w-1/2 flex justify-center">
+                <input type="date" id="datepicker" className="bg-[#2c2c2c] text-white dark:[color-scheme:dark]"/>
             </div>
         </div>
     )
